cd5ae6f869aa1f1360d14326a1d885819b5a598c
Youssef Mohamed Fekry Abdelghafar Nasr <yousseffekryk98@gmail.com>
2026-09-16 07:11:07 +0300

    fix: remove conflicting untyped React module declarations

--- src/types/declarations.d.ts
+++
@@ -1,5 +0,0 @@
-// Fallback module declarations to avoid requiring @types packages in this workspace
-declare module 'react';
-declare module 'react/jsx-runtime';
-declare module 'react-dom/client';
-declare module 'react-dom';
